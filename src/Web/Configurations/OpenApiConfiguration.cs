48c839a190ba95a0662dd30c07e1d89cae592ea8
Aadameqq <bryndza.ad@gmail.com>
2025-09-13 18:17:32 +0200

    added open api

--- src/Web/Configurations/OpenApiConfiguration.cs
+++ src/Web/Configurations/OpenApiConfiguration.cs
@@ -1,6 +1,44 @@
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
 namespace Web.Configurations;
 
-public class OpenApiConfiguration
+public static class OpenApiConfiguration
 {
-    
+	public static void SetUpOpenApi(this IServiceCollection services)
+	{
+		var info = new OpenApiInfo
+		{
+			Title = "Contest App API Definition",
+			Description = "Api definition for development purposes",
+			Version = "1.0",
+		};
+
+		services.AddEndpointsApiExplorer();
+		services.AddSwaggerGen(options =>
+		{
+			options.SwaggerDoc("docs", info);
+		});
+	}
+
+	public static void UseOpenApi(this WebApplication app)
+	{
+		if (!app.Environment.IsDevelopment())
+			return;
+
+		app.MapSwagger("/openapi/{documentName}.json");
+		app.MapScalarApiReference(
+			"/docs",
+			options =>
+			{
+				options
+					.WithDefaultOpenAllTags(false)
+					.WithClientButton(false)
+					.AddDocument("docs", "docs", "/openapi/docs.json")
+					.WithDocumentDownloadType(DocumentDownloadType.None)
+					.WithDefaultHttpClient(ScalarTarget.Node, ScalarClient.Fetch);
+				options.EnabledClients = [ScalarClient.Fetch, ScalarClient.Curl];
+			}
+		);
+	}
 }
