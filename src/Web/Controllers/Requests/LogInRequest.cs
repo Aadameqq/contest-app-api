f301fdefa4efa3fe7efa8c65e13f80d1cb620659
Aadameqq <bryndza.ad@gmail.com>
2025-09-14 13:48:41 +0200

    made rememberMe field not required

--- src/Web/Controllers/Requests/LogInRequest.cs
+++ src/Web/Controllers/Requests/LogInRequest.cs
@@ -4,5 +4,5 @@ public class LogInRequest
 {
 	public required string Email { get; init; }
 	public required string Password { get; init; }
-	public required bool RememberMe { get; init; }
+	public bool RememberMe { get; init; }
 }
